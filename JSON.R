ec8fcbd4e051b02139b4d583042d12dfb7637d3a
theleybzon <dannyleybzon@gmail.com>
2015-11-03 16:35:43 -0800

    Commented

--- JSON.R
+++ JSON.R
@@ -1,71 +1,104 @@
+###This program converts a slightly-modified JSON file obtained from sparql (a Wikipedia database query tool) to a .dot file, with formatting obtained from page 17 of http://www.graphviz.org/pdf/dotguide.pdf
+
+##Packages:
+#These lines will check if you have the necessary package installed, installs it if it is not already installed, and opens it
+list.of.packages <- c("bigrquery", "ggplot2", "methods", "dplyr", "grid")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+library(jsonlite)
+
+
+## Cleanup:
+#These lines create a  extract the three columns which have the info we require 
 json_file <- fromJSON("~/R/minicomedians.json")
 json_file <- json_file[2]
-results <- json_file$results
-json_file <- results$bindings
-json_file=data.frame(json_file$Comedian$value, json_file$influenced$value, json_file$birthDate$value)
+json_file <- json_file$results
+json_file <- json_file$bindings
+json_file <- data.frame(json_file$Comedian$value, json_file$influenced$value, json_file$birthDate$value)
+
+#These lines clean up the values in the first column we need
 json_file$json_file.Comedian.value<-gsub("^.*e/","",json_file$json_file.Comedian.value)
 json_file$json_file.Comedian.value<-gsub("_", " ", json_file$json_file.Comedian.value)
 
+#These lines clean up the values in the second column we need
 json_file$json_file.influenced.value<-gsub("^.*e/","",json_file$json_file.influenced.value)
 json_file$json_file.influenced.value<-gsub("_", " ", json_file$json_file.influenced.value)
 
+#These lines clean up the values in the third column we need
 json_file$json_file.birthDate.value<-gsub("-.*$", "", json_file$json_file.birthDate.value)
 json_file$json_file.birthDate.value<-strtoi(json_file$json_file.birthDate.value)
 
+#This line re-orders the values so that they are listed according to year of birth
 json_file<-json_file[order(json_file$json_file.birthDate.value),]
 
+#This line renames the columns so that they are easier to work with
 colnames(json_file) <- c("Name", "Influenced", "Year")
 
-min<-min(json_file$Year)
-max<-max(json_file$Year)
 
-year_rank<-data.frame()
-for(i in min:max) {
-  if (i %in% json_file$Year) {
-    year <- as.vector(subset(json_file, Year == i, select=Name))
-    if (ncol(year_rank)>0) {
-        year_rank<-data.frame(c(year_rank[,1:ncol(year_rank)], year))
-    } else {year_rank<-data.frame(year)}
-  }
+##Ranking:
+#This line creates a data frame "year_rank" which is filled with NAs, has the number of columns equal to the number of unique birthyears and rows equal to the maximum number of people in our dataset born in any given year
+year_rank<-as.data.frame(matrix(ncol=length(unique(json_file$Year)), nrow=max(table(json_file$Year))))
+#This line creates a counter variable b, which will iterate as the for-loop goes through
+b<-1
+#This for loop will iterate from the first birthyear of the first person in our dataset born to the birthyear of the last person born, and for each specified column adds the people who were born in that year
+for(i in unique(json_file$Year)) {
+  year <- as.vector(unique(subset(json_file, Year == i, select=Name)))
+  year_rank[1:dim(year)[1],b] <- year
+  b<-b+1
 }
 
+#This line renames the columns in the dataframe containing the rankings so that it's easier to visualize
 names(year_rank) <- unique(json_file$Year)
 
+#This line creates a character string which we will with lines containing a year and the list of all people born in that year
 year_char_string <- paste("")
+#These for loops fill the character string created above
 for(j in 1:ncol(year_rank)){
-year_char_string <- paste(year_char_string, "{ rank = same;", names(year_rank[j]))
-for(i in 1:nrow(year_rank)) {
+  year_char_string <- paste(year_char_string, "{ rank = same;", names(year_rank[j]))
+  for(i in 1:nrow(year_rank)) {
     if (!is.na(year_rank[i,j])){
         year_char_string <- paste(year_char_string, "; ", "\"", year_rank[i,j], "\"", sep="")
-    }
-}
-year_char_string <- paste(year_char_string, "; }\n", sep="")
+      }
+  }
+  year_char_string <- paste(year_char_string, "; }\n", sep="")
 }
 
+##Relationships:
+#This line creates a character string which we will fill with the relationships between people, with the format "Influencer -> Influenced"
 relationships<-paste("")
+#This line creates a vector which we will use in the next section, and in this section fill with all of the people who were not influenced by anybody
 ancestors_vector<-vector()
+#This line fills the character string and vector created above
 for(i in 1:nrow(json_file)){
   relationships<-paste(relationships, "\"", json_file$Name[i], "\"", " -> ", "\"", json_file$Influenced[i], "\"", ";\n", sep="")
-
   if(!(json_file$Name[i] %in% json_file$Influenced)){
     if(length(ancestors_vector)>0)
     {
       ancestors_vector<-c(ancestors_vector[1:length(ancestors_vector)], json_file$Name[i])
     } else {ancestors_vector<-c(json_file$Name[i])}
-    
   }
 }
 
+##Ancestors:
+#This line creates a character string which we will fill with all of the people who were not influenced by anybody
 ancestors <- paste("")
+#This line fills the character string with each person who was not influenced by anybody
 for(i in 1:length(unique(ancestors_vector))){
   ancestors <- paste(ancestors, "\"", unique(ancestors_vector)[i], "\"", "; ", sep="")
 }
 
+##Timeline:
+#This line creates a character string which contains the first year in which people were born
 timeline<-paste(unique(json_file$Year[1]))
+#This for loop adds to the character string created above, adding an arrow as well as the next year in which people were born
 for(i in 2:length(unique(json_file$Year))){
   timeline<-paste(timeline, " -> ", unique(json_file$Year)[i], sep="")
 }
+#This line adds a newline character to the end of this character string
 timeline<-paste(timeline, ";\n")
 
+##Final:
+#This line will combine all of the strings we have created, along with general formatting information
 final<-(paste("digraph asde91 { \n ranksep=.75; size = \"7.5,7.5\";\n\n { \nnode [shape=plaintext, fontsize=16];\n", timeline, ancestors, "\n}\n\n node [shape=box]; \n", year_char_string, "\n", relationships, "\n}"))
+#This line prints out the combination of all the strings we have created with general formatting information
 cat(final)
